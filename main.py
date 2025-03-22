dc7fb4bc8feca337ccec55cd9031bf01fc4d96bc
madhesh31 <armadhesh31@gmail.com>
2025-03-22 18:31:09 +0530

    base model

---
+++ main.py
@@ -0,0 +1,211 @@
+import datetime
+import os
+import sys
+import time
+import webbrowser
+import pyautogui
+import pyttsx3
+import speech_recognition as sr
+import json
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import psutil 
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+model = load_model("chat_model.h5")
+
+with open("tokenizer.pkl", "rb") as f:
+    tokenizer = pickle.load(f)
+
+with open("label_encoder.pkl", "rb") as encoder_file:
+    label_encoder = pickle.load(encoder_file)
+
+def initialize_engine():
+    engine = pyttsx3.init("sapi5") #sapi5 is a microsoft speech API
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id) #0=male 1=female
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate-40) #speed of speech
+    volume = engine.getProperty('volume')
+    engine.setProperty('volume', volume+0.5) #volume of speech increase
+    return engine
+
+def speak(text):
+    engine = initialize_engine()
+    engine.say(text)
+    engine.runAndWait()
+
+def command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=0.5)
+        print("Listening.......", end="", flush=True) #flush used to clear the buffer and replace line with next line of code prints)
+        r.pause_threshold=1.0 #seconds of non-speaking audio before a phrase is considered complete
+        r.phrase_threshold=0.3 #seconds of speaking audio before the phrase is considered complete
+        r.sample_rate = 48000
+        r.dynamic_energy_threshold=True
+        r.operation_timeout=5 #timeout after v speak
+        r.non_speaking_duration=0.5 #gap of words v speak
+        r.dynamic_energy_adjustment=2
+        r.energy_threshold=4000
+        r.phrase_time_limit = 10
+        # print(sr.Microphone.list_microphone_names())
+        audio = r.listen(source) #listen to the our commands
+    try:
+        print("\r" ,end="", flush=True) #flush used to clear the buffer and replace line with next line of code prints
+        print("Recognizing......", end="", flush=True) #flush at last makes listerning into recognizing and then user said!
+        query = r.recognize_google(audio, language='en-in') #recognize the audio #no bad words works here
+        print("\r" ,end="", flush=True)
+        print(f"User said : {query}\n")
+    except Exception:
+        print("Say that again please")
+        return "None"
+    return query
+
+def cal_day():
+    day = datetime.datetime.today().weekday() + 1 #+1 used to make monday as 1st day of week
+    day_dict={
+        1:"Monday",
+        2:"Tuesday",
+        3:"Wednesday",
+        4:"Thursday",
+        5:"Friday",
+        6:"Saturday",
+        7:"Sunday"
+    }
+    if day in day_dict.keys():
+        day_of_week = day_dict[day]
+        print(day_of_week)
+        return day_of_week
+    else:
+        return None
+
+def wishMe():
+    speak("Hello, I'm VIRSA")
+    hour = int(datetime.datetime.now().hour)
+    t = time.strftime("%I:%M:%p")
+    day = cal_day()
+
+    if(hour>=0) and (hour<=12) and ('AM' in t): #AM is mrng
+        speak(f"Good morning Mad, it's {day} and the time is {t}")
+    elif(hour>=12)  and (hour<=16) and ('PM' in t): #PM is afternoon
+        speak(f"Good afternoon Mad, it's {day} and the time is {t}")
+    else:
+        speak(f"Good evening Mad, it's {day} and the time is {t}")
+
+def social_media(command):
+    if 'facebook' in command:
+        speak("opening your facebook")
+        webbrowser.open("https://www.facebook.com/")
+    elif 'whatsapp' in command:
+        speak("opening your whatsapp")
+        webbrowser.open("https://web.whatsapp.com/")
+    elif 'discord' in command:
+        speak("opening your discord server")
+        webbrowser.open("https://discord.com/")
+    elif 'instagram' in command:
+        speak("opening your instagram")
+        webbrowser.open("https://www.instagram.com/")
+    else:
+        speak("No result found")
+
+def schedule():
+    day = cal_day().lower()
+    speak("Mad today's schedule is ")
+    week={
+    "monday": "Mad, from 9:00 to 9:50 you have Algorithms class, from 10:00 to 11:50 you have System Design class, from 12:00 to 2:00 you have a break, and today you have Programming Lab from 2:00 onwards.",
+    "tuesday": "Mad, from 9:00 to 9:50 you have Web Development class, from 10:00 to 10:50 you have a break, from 11:00 to 12:50 you have Database Systems class, from 1:00 to 2:00 you have a break, and today you have Open Source Projects lab from 2:00 onwards.",
+    "wednesday": "Mad, today you have a full day of classes. From 9:00 to 10:50 you have Machine Learning class, from 11:00 to 11:50 you have Operating Systems class, from 12:00 to 12:50 you have Ethics in Technology class, from 1:00 to 2:00 you have a break, and today you have Software Engineering workshop from 2:00 onwards.",
+    "thursday": "Mad, today you have a full day of classes. From 9:00 to 10:50 you have Computer Networks class, from 11:00 to 12:50 you have Cloud Computing class, from 1:00 to 2:00 you have a break, and today you have Cybersecurity lab from 2:00 onwards.",
+    "friday": "Mad, today you have a full day of classes. From 9:00 to 9:50 you have Artificial Intelligence class, from 10:00 to 10:50 you have Advanced Programming class, from 11:00 to 12:50 you have UI/UX Design class, from 1:00 to 2:00 you have a break, and today you have Capstone Project work from 2:00 onwards.",
+    "saturday": "Mad, today you have a more relaxed day. From 9:00 to 11:50 you have team meetings for your Capstone Project, from 12:00 to 12:50 you have Innovation and Entrepreneurship class, from 1:00 to 2:00 you have a break, and today you have extra time to work on personal development and coding practice from 2:00 onwards.",
+    "sunday": "Mad, today is a holiday, but keep an eye on upcoming deadlines and use this time to catch up on any reading or project work."
+    }
+    if day in week.keys():
+        speak(week[day])
+
+def openApp(command):
+    if "calculator" in command:
+        speak("opening calculator")
+        os.startfile('C:\\Windows\\System32\\calc.exe')
+    elif "notepad" in command:
+        speak("opening notepad")
+        os.startfile('C:\\Windows\\System32\\notepad.exe')
+    elif "paint" in command:
+        speak("opening paint")
+        os.startfile('C:\\Windows\\System32\\mspaint.exe')
+
+def closeApp(command):
+    if "calculator" in command:
+        os.system("taskkill /f /im CalculatorApp.exe")
+        speak("closing calculator")
+    elif "notepad" in command:
+        speak("closing notepad")
+        os.system('taskkill /f /im notepad.exe')
+    elif "paint" in command:
+        speak("closing paint")
+        os.system('taskkill /f /im mspaint.exe')
+
+def browsing(query):
+    if 'google' in query:
+        speak("Boss, what should I search on google?")
+        s = command().lower()
+        if s.lower() != "none":
+            webbrowser.open(f"https://www.google.com/search?q={s}")
+
+def condition():
+    usage = str(psutil.cpu_percent())
+    speak(f"CPU is at {usage} percentage")
+    battery = psutil.sensors_battery()
+    percentage = battery.percent
+    speak(f"Boss our system have {percentage} percentage battery")
+
+    if percentage>=80:
+        speak("Boss we could have enough charging to continue our recording")
+    elif percentage>=40 and percentage<=75:
+        speak("Boss we should connect our system to charging point to charge our battery")
+    else:
+        speak("Boss we have very low power, please connect to charging otherwise recording should be off...")
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = command().lower()
+        print(query)
+        # query  = input("Enter your command-> ")
+        if ('facebook' in query) or ('discord' in query) or ('whatsapp' in query) or ('instagram' in query):
+            social_media(query)
+        elif ("university time table" in query) or ("schedule" in query):
+            schedule()
+        elif ("volume up" in query) or ("increase volume" in query):
+            pyautogui.press("volumeup")
+            speak("Volume increased")
+        elif ("volume down" in query) or ("decrease volume" in query):
+            pyautogui.press("volumedown")
+            speak("Volume decreased")
+        elif ("volume mute" in query) or ("mute the sound" in query):
+            pyautogui.press("volumemute")
+            speak("Volume muted")
+        elif ("open calculator" in query) or ("open notepad" in query) or ("open paint" in query):
+            openApp(query)
+        elif ("close calculator" in query) or ("close notepad" in query) or ("close paint" in query):
+            closeApp(query)
+        elif ("what" in query) or ("who" in query) or ("how" in query) or ("hi" in query) or ("thanks" in query) or ("hello" in query):
+                padded_sequences = pad_sequences(tokenizer.texts_to_sequences([query]), maxlen=20, truncating='post')
+                result = model.predict(padded_sequences)
+                tag = label_encoder.inverse_transform([np.argmax(result)])
+
+                for i in data['intents']:
+                    if i['tag'] == tag:
+                        speak(np.random.choice(i['responses']))
+        elif ("open google" in query):
+            browsing(query)
+        elif ("system condition" in query) or ("condition of the system" in query):
+            speak("cheking system condition")
+            condition()
+        elif "exit" in query:
+            sys.exit()
